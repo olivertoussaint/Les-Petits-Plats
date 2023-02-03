0ccbd0acb9982cde16e97e88c087a107cdb900a4
olivertoussaint <46441581+olivertoussaint@users.noreply.github.com>
2023-02-03 16:51:15 +0100

    adding new file

--- js/search.js
+++ js/search.js
@@ -1,11 +1,13 @@
-import { recipes } from './../data/recipes.js'
+import { recipes } from '../data/recipes.js'
+import { createAListFactory, createList } from './Factories/listFactories.js'
 import { createARecipeFactory } from './Factories/recipefactories.js'
 
 // ----------------- DOM
-const recipesContainer = document.getElementById('recipes-container')
+const recipesContainer = document.getElementById('resultRecipes-container')
 const noResultsContainer = document.getElementById('noResults')
 
 // ----------------- Fonctions
+// affiche les recettes une par une à partir d'un array de recettes filtré ou non
 function displayRecipes (array) {
   recipesContainer.innerHTML = ''
   if (array.length !== 0) {
@@ -16,5 +18,16 @@ function displayRecipes (array) {
   }
 }
 
+// affiche les boutons de filtres avec leur titre
+function displayListButtons (array) {
+  const buttonsEntitled = createList(array)
+  buttonsEntitled.forEach(element => createAListFactory().getListBlock(element))
+  console.log(buttonsEntitled)
+  return buttonsEntitled
+}
+
+
 // ----------------- APPEL des fonctions
+
+displayListButtons(recipes)
 displayRecipes(recipes)
